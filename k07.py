a38863a3416a7eb97cd4654cf8aa81a1698520ae
Bayan Berri <bberri@stuy.edu>
2017-10-04 16:31:08 -0400

    done

--- k07.py
+++ k07.py
@@ -7,8 +7,13 @@ app.secret_key = os.urandom(32)
 username = "bob"
 password = "bobbins"
 
-@app.route("/")
+@app.route("/",methods=['POST','GET'])
 def hello_world():
+    '''
+    checks user info and displays greet or login
+
+    returns html page greet or login 
+    '''
     print "\n\n"
     print "Cookie username: ", session.get('username')
     print "Cookie password: ",session.get('password')
@@ -17,20 +22,30 @@ def hello_world():
     else:
         return render_template( 'login.html' )
 
-@app.route("/greet")
+@app.route("/greet",methods=['POST','GET'])
 def greet():
+    '''
+    sets user info keys
+
+    returns greet or error html pages 
+    '''
     print "\n\n"
-    print "Form username: ", request.args.get('username')
-    print "Form password: ", request.args.get('password')
-    session['username'] = request.args.get('username')
-    session['password'] = request.args.get('password')
+    print "Form username: ", request.form.get('username')
+    print "Form password: ", request.form.get('password')
+    session['username'] = request.form.get('username')
+    session['password'] = request.form.get('password')
     if session.get('username')== username and session.get('password')==password:
         return render_template( 'greet.html', user = session.get('username') )
     else:
         return render_template('error.html')
 
-@app.route("/logout")
+@app.route("/logout",methods=['POST','GET'])
 def logout():
+    '''
+    clears cookies
+
+    returns login html page
+    '''
     session.pop("username")
     session.pop("password")
     return render_template('login.html')
